e0ea93329ef7701a8be6146dcfc87f2306f1bba1
Andrew Brazhenko <andreybrazhenko@yandex.ru>
2020-05-07 14:53:33 +0300

    fix +

--- sources/main_wrappers.c
+++ sources/main_wrappers.c
@@ -6,7 +6,7 @@
 /*   By: a17641238 <a17641238@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/15 00:14:30 by a17641238         #+#    #+#             */
-/*   Updated: 2020/05/07 12:04:52 by a17641238        ###   ########.fr       */
+/*   Updated: 2020/05/07 14:45:54 by a17641238        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,10 @@ void		*malloc(size_t size)
 	void		*ret;
 	write(2, "aloc\n", 5);
 
-	//fprintf(stderr, "aloc: %zu\n", size);
 	lock_();
 	ret = malloc_(size);
 	unlock_();
 	mem_dump();
-	//fprintf(stderr, "%zu %p\n", size, ret);
 	return (ret);
 }
 
@@ -39,7 +37,7 @@ void		*realloc(void *ptr, size_t size)
 	void		*ret;
 
 	write(2, "real\n", 5);
-	//fprintf(stderr, "real: %zu, %p\n", size, ptr);
+
 	lock_();
 	ret = realloc_(ptr, size);
 	unlock_();
@@ -55,7 +53,6 @@ void		free(void *ptr)
 {
 	write(2, "free\n", 5);
 
-//  printf("1");
 	lock_();
 	free_(ptr);
 	unlock_();
